70e82e626d0da4261ca6989f32bbd35395c7d002
teemal <taymal1987@gmail.com>
2019-10-19 13:30:23 -0700

    got d3 to bind to DOM object and moved script into own js file

--- index.js
+++ index.js
@@ -1,46 +1,30 @@
-// var data = [30, 86, 168, 281, 303, 365];
+var svgWidth = 500;
+      var svgHeight = 300;
 
-// d3.select(".example_graph")
-//   .selectAll("div")
-//   .data(data)
-//   .enter()
-//   .append("div")
-//   .style("width", function(d) {
-//     return d + "px";
-//   })
-//   .text(function(d) {
-//     return d;
-//   },
-// //   console.log("FUCK fuck FUCK")
-//   );
+      var svg = d3
+        .select("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+        .attr("class", "bar-chart");
 
-var svgWidth = 500;  
-var svgHeight = 300;
+      var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-var svg = d3.select('svg')  
-    .attr("width", svgWidth)  
-    .attr("height", svgHeight)  
-    .attr("class", "bar-chart");
+      var barPadding = 5;
+      var barWidth = svgWidth / dataset.length;
 
-
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-
-var barPadding = 5;
-var barWidth = svgWidth / dataset.length;
-
-var barChart = svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("y", function(d) {
-    return svgHeight - d;
-  })
-  .attr("height", function(d) {
-    return d;
-  })
-  .attr("width", barWidth - barPadding)
-  .attr("transform", function(d, i) {
-    var translate = [barWidth * i, 0];
-    return "translate(" + translate + ")";
-  });
+      var barChart = svg
+        .selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("y", function(d) {
+          return svgHeight - d;
+        })
+        .attr("height", function(d) {
+          return d;
+        })
+        .attr("width", barWidth - barPadding)
+        .attr("transform", function(d, i) {
+          var translate = [barWidth * i, 0];
+          return "translate(" + translate + ")";
+        });
