08ba2ca578c87cabf5b073c0f32b2b7e72f3b9df
teemal <taymal1987@gmail.com>
2019-10-19 13:23:05 -0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-var data = [30, 86, 168, 281, 303, 365];
+// var data = [30, 86, 168, 281, 303, 365];
 
-d3.select(".example_graph")
-  .selectAll("div")
-  .data(data)
-  .enter()
-  .append("div")
+// d3.select(".example_graph")
+//   .selectAll("div")
+//   .data(data)
+//   .enter()
+//   .append("div")
 //   .style("width", function(d) {
 //     return d + "px";
 //   })
-.style(width = 500, height = 500)
-  .text(function(d) {
-    return d;
-  },
-//   console.log("FUCK fuck FUCK")
-  );
+//   .text(function(d) {
+//     return d;
+//   },
+// //   console.log("FUCK fuck FUCK")
+//   );
 
+var svgWidth = 500;  
+var svgHeight = 300;
+
+var svg = d3.select('svg')  
+    .attr("width", svgWidth)  
+    .attr("height", svgHeight)  
+    .attr("class", "bar-chart");
+
+
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var barPadding = 5;
+var barWidth = svgWidth / dataset.length;
+
+var barChart = svg
+  .selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("y", function(d) {
+    return svgHeight - d;
+  })
+  .attr("height", function(d) {
+    return d;
+  })
+  .attr("width", barWidth - barPadding)
+  .attr("transform", function(d, i) {
+    var translate = [barWidth * i, 0];
+    return "translate(" + translate + ")";
+  });
