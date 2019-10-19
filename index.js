9f30eb3bc748ab85f2f460c53288bee7cdb84a6f
teemal <taymal1987@gmail.com>
2019-10-19 15:10:53 -0700

    testing json input

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-var svgWidth = 500;
-var svgHeight = 300;
+const svg = d3.select("svg"),
+  margin = { top: 20, right: 20, bottom: 30, left: 40 },
+  width = +svg.attr("width") - margin.left - margin.right,
+  height = +svg.attr("height") - margin.top - margin.bottom,
+  x = d3
+    .scaleBand()
+    .rangeRound([0, width])
+    .padding(0.2),
+  y = d3.scaleLinear().rangeRound([height, 0]),
+  g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-var svg = d3
-  .select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
-  .attr("class", "bar-chart");
+d3.json("fake_data.json").then(data => {
+  data = data.county;
 
-// var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-d3.json("fake_data.json", function(error, data) {
-  data.forEach(function(d) {
-    d.county = d.county;
-    d.county.years = d.county.crook.year;
-    console.log(d.county);
-    console.log(d.county.years);
-  });
+  x.domain(data.map(d => d.genre));
+  y.domain([0, d3.max(data, d => d.year)]);
 
-  console.log("+++++++++++++++++++++++++++++++++");
-  console.log(error);
-  console.log(data);
-  console.log(data.county);
-  console.log(data.county.crook);
-  console.log(data.county.crook.year[1999]);
-  var barPadding = 5;
-  var barWidth = svgWidth / dataset.length;
+  g.append("g")
+    .attr("class", "axis axis-x")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
 
-  var barChart = svg
-    .selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-      return svgHeight - d;
-    })
-    .attr("height", function(d) {
-      return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function(d, i) {
-      var translate = [barWidth * i, 0];
-      return "translate(" + translate + ")";
-    });
+  g.append("g")
+    .attr("class", "axis axis-y")
+    .call(d3.axisLeft(y).ticks(10));
 });
+
+g.selectAll(".bar")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("class", "bar")
+  .attr("x", d => x(d.genre))
+  .attr("y", d => y(d.count))
+  .attr("width", x.bandwidth())
+  .attr("height", d => height - y(d.count));
