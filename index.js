ee980a7258d0eb21cc15d151c230acd21039bd34
teemal <taymal1987@gmail.com>
2019-10-19 14:36:58 -0700

    testing json input

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var svg = d3.select('svg')
 var dataset = d3.json("fake_data.json", function(data) {
     console.log(data);
     console.log(data.county);
-    console.log(data.county.year);
+    console.log(data.county.year[1999]);
 });
 
 var barPadding = 5;
