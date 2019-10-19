b21263e22a0332b9d1cc7aba697d40bc614e33a4
teemal <taymal1987@gmail.com>
2019-10-19 14:55:46 -0700

    testing json input

--- index.js
+++ index.js
@@ -1,60 +1,44 @@
-// var data = [30, 86, 168, 281, 303, 365];
-
-// d3.select(".example_graph")
-//   .selectAll("div")
-//   .data(data)
-//   .enter()
-//   .append("div")
-//   .style("width", function(d) {
-//     return d + "px";
-//   })
-//   .text(function(d) {
-//     return d;
-//   },
-// //   console.log("FUCK fuck FUCK")
-//   );
-
-var svgWidth = 500;  
+var svgWidth = 500;
 var svgHeight = 300;
 
-var svg = d3.select('svg')  
-    .attr("width", svgWidth)  
-    .attr("height", svgHeight)  
-    .attr("class", "bar-chart");
-
+var svg = d3
+  .select("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+  .attr("class", "bar-chart");
 
 // var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 d3.json("fake_data.json", function(error, data) {
-    data.array.forEach(function(d) {
-        d.county = d.county;
-        d.county.years = d.county.crook.year;
-        console.log(d.county);
-        console.log(d.county.years);
-    });
-    console.log("+++++++++++++++++++++++++++++++++")
-    console.log(error)
-    console.log(data);
-    console.log(data.county);
-    console.log(data.county.crook);
-    console.log(data.county.crook.year[1999]);
-});
-
-var barPadding = 5;
-var barWidth = svgWidth / dataset.length;
-
-var barChart = svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("y", function(d) {
-    return svgHeight - d;
-  })
-  .attr("height", function(d) {
-    return d;
-  })
-  .attr("width", barWidth - barPadding)
-  .attr("transform", function(d, i) {
-    var translate = [barWidth * i, 0];
-    return "translate(" + translate + ")";
+  data.forEach(function(d) {
+    d.county = d.county;
+    d.county.years = d.county.crook.year;
+    console.log(d.county);
+    console.log(d.county.years);
   });
+
+  console.log("+++++++++++++++++++++++++++++++++");
+  console.log(error);
+  console.log(data);
+  console.log(data.county);
+  console.log(data.county.crook);
+  console.log(data.county.crook.year[1999]);
+  var barPadding = 5;
+  var barWidth = svgWidth / dataset.length;
+
+  var barChart = svg
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+      return svgHeight - d;
+    })
+    .attr("height", function(d) {
+      return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+      var translate = [barWidth * i, 0];
+      return "translate(" + translate + ")";
+    });
+});
